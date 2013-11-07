a4af2f1e29a8b8855b4fa00d5e20d049d19c6da9
Hank Yates <hankyates@gmail.com>
2013-11-07 07:55:50 +0000

    adding redis to cache recipes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var restify = require('restify'),
     http = require('http'),
     api_key = require('./api_key.json'),
     api_url = 'http://api.yummly.com/v1/api/recipes?_app_id=' + api_key.application_id + '&_app_key=' + api_key.application_key + '&q=',
+    redis = require('redis'),
+    redis_client = redis.createClient(),
     server = restify.createServer({ name: 'friedDog' });
 
 server.use(restify.acceptParser(server.acceptable));
@@ -11,26 +13,45 @@ server.use(restify.bodyParser());
 server.get('/recipes/:term', function (req, res, next) {
   var recipes = {};
 
-  http.get(api_url + req.params.term, function(response) {
-    var recipes_raw = '';
 
-    response.on('data', function (chunk){
-        recipes_raw += chunk;
-    });
-
-    response.on('end',function(){
-        recipes = JSON.parse(recipes_raw);
-        res.send(recipes);
-        return next();
-    });
-
-  }).on('error', function(e) {
-    res.send(e.message);
+  redis_client.on('error', function (err) {
+      console.log('Error ' + err);
   });
 
+  redis_client.hmget(req.params.term, function (err, replies) {
+    if (!replies) {
+      recipes = replies;
+    } else {
+      res.send(replies);
+      return next();
+    }
+  });
+
+  if (!recipes.length) {
+    http.get(api_url + req.params.term, function(response) {
+      var recipes_raw = '';
+
+      response.on('data', function (chunk){
+          recipes_raw += chunk;
+      });
+
+      response.on('end',function(){
+          recipes = JSON.parse(recipes_raw);
+          redis_client.hmset(req.params.term, recipes);
+          res.send(recipes);
+          return next();
+      });
+    }).on('error', function(e) {
+      res.send(e.message);
+    });
+  }
 
 });
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
+});
+
+process.on('exit', function() {
+  redis_client.quit();
 });
