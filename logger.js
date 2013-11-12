852c847c9ae49b9f76aae4c7c8a42b24a2f2b511
Hank Yates <hankyates@gmail.com>
2013-11-12 05:06:08 +0000

    adding wrapper for redis. there is some cleanup to do here though.

--- logger.js
+++ logger.js
@@ -1,41 +1,41 @@
 function logger(moduleName) {
-	var self = this;
-	this.logLevels = {info:0,warn:1,error:2,fatal:3};
-	this.level = 0;
+  var self = this;
+  this.logLevels = {info:0,warn:1,error:2,fatal:3};
+  this.level = 0;
 
-	function logMsgAtLevel(msg,msgLevel,levelText) {
-		if( self.level <= msgLevel ) {
-			console.log('[' + levelText + '] ' + moduleName + ' > ' + msg);
-		}
-	}
+  function logMsgAtLevel(msg,msgLevel,levelText) {
+    if( self.level <= msgLevel ) {
+      console.log('[' + levelText + '] ' + moduleName + ' > ' + msg);
+    }
+  }
 
-	this.info = function(msg) {
-		logMsgAtLevel(msg,self.logLevels.info,'info');
-	}
+  this.info = function(msg) {
+    logMsgAtLevel(msg,self.logLevels.info,'info');
+  }
 
-	this.warn = function(msg) {
-		logMsgAtLevel(msg,self.logLevels.warn,'warn');
-	}
+  this.warn = function(msg) {
+    logMsgAtLevel(msg,self.logLevels.warn,'warn');
+  }
 
-	this.error = function(msg) {
-		logMsgAtLevel(msg,self.logLevels.error,'error');
-	}
+  this.error = function(msg) {
+    logMsgAtLevel(msg,self.logLevels.error,'error');
+  }
 
-	this.fatal = function(msg) {
-		logMsgAtLevel(msg,self.logLevels.fatal,'fatal');
-	}
+  this.fatal = function(msg) {
+    logMsgAtLevel(msg,self.logLevels.fatal,'fatal');
+  }
 
-	this.setLevel = function(lvl) {
-		self.level = ( lvl < self.logLevels.info || lvl > self.logLevels.fatal )
-			? self.logLevels.info
-			: lvl;
-	}
+  this.setLevel = function(lvl) {
+    self.level = ( lvl < self.logLevels.info || lvl > self.logLevels.fatal )
+      ? self.logLevels.info
+      : lvl;
+  }
 
-	return this;
+  return this;
 }
 
 
 exports = module.exports = {
-	Logger: logger
+  Logger: logger
 };
 
