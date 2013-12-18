1cc708ce467d0234226d753bbe2d92cd7caa3faf
Hank Yates <hankyates@gmail.com>
2013-12-18 12:56:30 -0800

    Update recipe.js

--- public/js/models/recipe.js
+++ public/js/models/recipe.js
@@ -1,7 +1,6 @@
 define([], function() {
   var Recipe = Backbone.Model.extend({
     initialize: function(recipe) {
-      console.log(recipe);
       this.set({
         name: recipe.recipeName,
         imageUrl: recipe.smallImageUrls[0]
